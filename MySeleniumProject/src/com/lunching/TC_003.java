81286946f09ebd9b205ae1faa59950dc0d6d78f9
subha2022 <subhasmitadoll@gmail.com>
2022-01-10 19:36:22 +0530

    print some msg in TC_003

--- MySeleniumProject/src/com/lunching/TC_003.java
+++ MySeleniumProject/src/com/lunching/TC_003.java
@@ -2,8 +2,9 @@ package com.lunching;
 
 public class TC_003 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("haiii");
 
 	}
 
