3ef53b8cdbb21488d8d1b09dcf4c98c0ead6f26d
subha2022 <subhasmitadoll@gmail.com>
2022-02-18 08:56:24 +0530

    Maven TestNG Program Added

---
+++ MavenTestNGNew/src/test/java/MavenNewProject/MavenTestNGNew/Test_001.java
@@ -0,0 +1,20 @@
+package MavenNewProject.MavenTestNGNew;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test_001 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver  = new ChromeDriver();
+		driver.get("https://www.gmail.com");
+		driver.close();
+
+	}
+
+}
