82ffb4975a1c81ef50b3d874eec1ea9eb6c108db
subha2022 <subhasmitadoll@gmail.com>
2022-03-22 13:14:26 +0530

    Java interview program

---
+++ JavaProgramPractice/src/javaInterviewProgram/VowelAndConsonant.java
@@ -0,0 +1,22 @@
+package javaInterviewProgram;
+
+public class VowelAndConsonant {
+
+	public static void main(String[] args) 
+	{
+		// a,e.,i,o,u
+		
+		char ch ='b';
+		
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println("This is vowel: "+ch);
+		}else
+		{
+			System.out.println("This is consonant: "+ch);
+		}
+		
+
+	}
+
+}
