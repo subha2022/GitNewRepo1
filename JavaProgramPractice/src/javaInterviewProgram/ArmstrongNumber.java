0012a6f622e54405610c8f161682ec003305abdc
subha2022 <subhasmitadoll@gmail.com>
2022-03-24 20:43:00 +0530

    java interview program updated

---
+++ JavaProgramPractice/src/javaInterviewProgram/ArmstrongNumber.java
@@ -0,0 +1,23 @@
+package javaInterviewProgram;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) 
+	{
+		int n=155;
+		int temp =n;
+		int r,sum=0;
+		
+		while(n>0)
+		{
+			r=n%10;
+			n=n/10;
+			sum=sum+r*r*r;
+		}
+		if(temp==sum)
+			System.out.println("Its a Armstrong Number");
+		else
+			System.out.println("Not an Armstrong Number");
+	}
+
+}
