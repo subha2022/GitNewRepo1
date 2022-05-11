e3923c6ac89f9655627b60e188ef7c82bbf135a3
subha2022 <subhasmitadoll@gmail.com>
2022-05-11 13:17:14 +0530

    java program updated

--- JavaProgramPractice/src/javaInterviewProgram/PracticeJavaProgram.java
+++ JavaProgramPractice/src/javaInterviewProgram/PracticeJavaProgram.java
@@ -1,32 +1,27 @@
 package javaInterviewProgram;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class PracticeJavaProgram {
 
 	public static void main(String[] args) 
 	{
-		int num=4;
-		int count=0;
-		if(num>1)
-		{
-			for(int i=1;i<=num;i++)
-			{
-				if(num%i==0)
-				count++;
-			}
-			if(count==2)
-			{
-				System.out.println("prime no");
-			}else
-			{
-				System.out.println("not a prime no");
-			}
-		}
-		else
-		{
-			System.out.println("not a prime no");
-		}
-		
-}
-}
+	Scanner sc= new Scanner(System.in);
+	 System.out.println("enter a no: ");
+	 int num =sc.nextInt();
+	 
+	int ornum=num;
+	int rev=0;
+	while(num!=0)
+	{
+		rev=rev*10+num%10;
+		num=num/10;
+	}
+	System.out.println(rev);
+	
+	 }
+	 
+	
+	}
+
--- JavaProgramPractice/src/javaInterviewProgram/SumofArrayElement.java
+++ JavaProgramPractice/src/javaInterviewProgram/SumofArrayElement.java
@@ -4,7 +4,7 @@ public class SumofArrayElement {
 
 	public static void main(String[] args)
 	{
-		int a[] = {1,2,4,5};
+		int a[] = {1,2,3,4};
 		
 		int sum = 0;
 		
