969967f70e9eb0aa085c03b861f358ba1cd6d31b
subha2022 <subhasmitadoll@gmail.com>
2022-06-02 13:41:47 +0530

    Screenshot program updated

--- JavaProgramPractice/src/javaInterviewProgram/PracticeJavaProgram.java
+++ JavaProgramPractice/src/javaInterviewProgram/PracticeJavaProgram.java
@@ -1,27 +1,31 @@
 package javaInterviewProgram;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class PracticeJavaProgram {
 
 	public static void main(String[] args) 
 	{
-	Scanner sc= new Scanner(System.in);
-	 System.out.println("enter a no: ");
-	 int num =sc.nextInt();
-	 
-	int ornum=num;
-	int rev=0;
-	while(num!=0)
-	{
-		rev=rev*10+num%10;
-		num=num/10;
+		/*Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a num: ");
+		int num=sc.nextInt();*/
+		
+		String name="Subhasmita";
+		String rev=" ";
+		int len=name.length();
+		System.out.println(len);
+		for(int i=len-1;i>=0;i--)//9 8
+		{
+			rev=rev+name.charAt(i);// 9
+		}
+		System.out.println(rev);
+		
+		
+		
+		
+			
+		
+		
+		
 	}
-	System.out.println(rev);
-	
-	 }
-	 
-	
-	}
-
+}
