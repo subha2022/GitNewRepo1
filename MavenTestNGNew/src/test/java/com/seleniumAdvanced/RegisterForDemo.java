61dab48f17bea0d5e73825dde14e06ed83048605
subha2022 <subhasmitadoll@gmail.com>
2022-10-14 22:15:41 +0530

    Registration for Demo progarm is completed

--- MavenTestNGNew/src/test/java/com/seleniumAdvanced/RegisterForDemo.java
+++ MavenTestNGNew/src/test/java/com/seleniumAdvanced/RegisterForDemo.java
@@ -1,5 +1,7 @@
 package com.seleniumAdvanced;
 
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,10 +24,11 @@ public class RegisterForDemo {
 		String countryName="India";
 		String emailId="subhasmitadoll@gmail.com";
 		String dateOfDemo="10/15/2022";
-		String houtInput="08";
+		String hourInput="08";
 		String mintInput="36";
 		String ampmInput="am";
 		String mobileNumber="7853860002";
+		String course="Selenium WebDriver";
 		String enterYourQuery="Which Topics are included in Selenium class.";
 		String successfullMessageText="Registration form is successfully submited.";
 
@@ -48,11 +51,12 @@ public class RegisterForDemo {
 		String dateidloc="vfb-18";
 		String hourNameLoc="vfb-16[hour]";
 		String minIdLoc="vfb-16-min";
-		String monNameLoc="vfb-19";
+		String mobNameLoc="vfb-19";
 		String courseNameLoc="vfb-20[]";
-		String queryIdLoc="item-vfb-23";
+		String queryIdLoc="vfb-23";
 		String verificationIdLoc="vfb-3";
 		String submitBtnNameLoc="vfb-submit";
+		String successMsgxpath="//*[@id=\"content\"]/article/div/div/section[1]/div/div[1]/div/div/div/div";
 
 
 
@@ -220,9 +224,145 @@ public class RegisterForDemo {
 			System.out.println("The Country Address is not displayed");
 		}
 
+		//Email
+		WebElement emailData = driver.findElement(By.name(emailnameloc));
+		emailData.sendKeys(emailId);
+		//Validating email Id is displayed or not
+		if(emailData.isDisplayed())
+		{
+			System.out.println("Email Id is displayed");
+			System.out.println("The Email Id value is:"+emailId);
+		}else
+		{
+			System.out.println("Email Id is not displayed");
+		}
 
+		//Date of Demo
+		WebElement dateData = driver.findElement(By.id(dateidloc));
+		dateData.sendKeys(dateOfDemo);
+		//Validating the date is displayed or not
+		if(dateData.isDisplayed())
+		{
+			System.out.println("Date is displayed");
+			System.out.println("The date value is:"+dateOfDemo);
+		}else
+		{
+			System.out.println("Date is not displayed");
+		}
 
+		//Convenient Time
+		//for hour
+		WebElement hourData = driver.findElement(By.name(hourNameLoc));
+		hourData.sendKeys(hourInput);
+		//Validating hour is displayed or not
+		if(hourData.isDisplayed())
+		{
+			System.out.println("Hour is displayed");
+			System.out.println("The Hour Value is:"+hourInput);
+		}else
+		{
+			System.out.println("Hour is not displayed");
+		}
 
+		//for minute
+		WebElement minData = driver.findElement(By.id(minIdLoc));
+		minData.sendKeys(mintInput);
+		//Validating hour is displayed or not
+		if(hourData.isDisplayed())
+		{
+			System.out.println("Minute is displayed");
+			System.out.println("The Minute Value is:"+mintInput);
+		}else
+		{
+			System.out.println("Minute is not displayed");
+		}
+
+		//Mobile Number
+		WebElement mobData = driver.findElement(By.name(mobNameLoc));
+		mobData.sendKeys(mobileNumber);
+		//Validating Mobile Number is displayed or not
+		if(mobData.isDisplayed())
+		{
+			System.out.println("Mobile Number is displayed");
+			System.out.println("The Mobile Number is:"+mobileNumber);
+		}else
+		{
+			System.out.println("Mobile Number is not displayed");
+		}
+		
+		//Click Which Course are you Interested
+		WebElement courseData = driver.findElement(By.name(courseNameLoc));
+		courseData.sendKeys(course);
+		//Validating Selenium Button is clicked or not
+		if(courseData.isDisplayed())
+		{
+			System.out.println("The Course is displayed");
+			System.out.println("The Course is:"+course);
+		}else
+		{
+			System.out.println("The Course is not displayed");
+		}
+		
+		//Enter your query
+		WebElement queryData = driver.findElement(By.id(queryIdLoc));
+		queryData.sendKeys(enterYourQuery);
+		//Validating Query is displayed or not
+		if(queryData.isDisplayed())
+		{
+			System.out.println("The Query is displayed");
+			System.out.println("The Query Value is:"+enterYourQuery);
+		}else
+		{
+			System.out.println("The Query is not displayed");
+		}
+		
+		//Verification
+		WebElement VerificationDigit = driver.findElement(By.id(verificationIdLoc));
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a two digit number:");
+		int VerificationCode=sc.nextInt();
+		System.out.println(VerificationCode);
+		
+		//Submit Button
+		WebElement subBtn = driver.findElement(By.name(submitBtnNameLoc));
+		 //Validating click button is enabled
+		if(subBtn.isEnabled())
+		{
+			System.out.println("Submit Button is enabled");
+			subBtn.click();
+			System.out.println("Submit Button is clicked");
+		}
+		Thread.sleep(3000);
+		
+		//verify the successful Message is displayed or not
+		WebElement successMsgText = driver.findElement(By.xpath(successMsgxpath));
+		if(successMsgText.isDisplayed())
+		{
+			System.out.println("Successful Message is displayed");
+			System.out.println("The successful message is:"+successfullMessageText);
+		}else
+		{
+			System.out.println("Successful Message is not displayed");
+		}
+		Thread.sleep(3000);
+		//Validating the Message 
+		String actsuccessMsg=successMsgText.getText();
+		String expSuccessMsg="Registration Form is Successfully Submitted. The Transaction ID : NXTGEN1937465";
+		//Validating successful message
+		if(actsuccessMsg.endsWith(expSuccessMsg))
+		{
+			System.out.println("Both the Messages are same");
+			System.out.println("The actual successful message is:"+actsuccessMsg);
+		}else
+		{
+			System.out.println("Both the Messages are not same");
+			System.out.println("The actual successful message is:"+actsuccessMsg);
+			System.out.println("The actual successful message is:"+expSuccessMsg);
+		}
+		//close the application
+		driver.close();
+		System.out.println("The Program is executed successfully");
+		
 	}
 
 }
