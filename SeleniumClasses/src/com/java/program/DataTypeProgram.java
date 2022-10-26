e81e65d85013540696012a2f474b67660c7a6e79
subha2022 <subhasmitadoll@gmail.com>
2022-10-26 21:56:07 +0530

    3 java assignment is completed

---
+++ SeleniumClasses/src/com/java/program/DataTypeProgram.java
@@ -0,0 +1,51 @@
+package com.java.program;
+
+public class DataTypeProgram 
+{
+
+public static void main(String[] args) 
+{
+	//Multiline Comments
+	/* 8 primitive data types are
+	 * byte,short, int ,long, float , double- Numeric data type
+	 * boolean, char- Non numeric data type 
+	 */
+	
+	//byte data type - Single line comment
+	byte rollNo=12;
+	System.out.println("Byte data type value is:"+rollNo);
+	
+	//short data type
+	short temprature=-200;
+	System.out.println("Short data type value is:"+temprature);
+	
+	//integer data type
+	int empId=254367;
+	System.out.println("Integer data type value is:"+empId);
+	
+	// long data type
+	long range=1245678647l;
+	System.out.println("Long data type value is:"+range);
+	
+	//float data type
+	float salary=45.678f;
+	System.out.println("Float data type value is:"+salary);
+	
+	//double data type
+	double number=45.23;
+	System.out.println("Double data type value is:"+number);
+	
+	//boolean data type
+	boolean flag=true;
+	System.out.println("Boolean data type value is:"+flag);
+	
+	//char data type
+	char letter='S';
+	System.out.println("Character data type value is:"+letter);
+	
+	//String data type
+	String name="Subhasmita";
+	System.out.println("String data type value is:"+name);
+	
+}
+}
