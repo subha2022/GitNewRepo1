99da7f0eb3601bcf0da8774ae6aa640c6ebad291
subha2022 <subhasmitadoll@gmail.com>
2023-02-07 20:57:41 +0530

    Handling Web Table Program.

--- SeleniumClasses/src/com/java/program/ForLoop_NestedForLoop.java
+++ SeleniumClasses/src/com/java/program/ForLoop_NestedForLoop.java
@@ -4,13 +4,22 @@ public class ForLoop_NestedForLoop {
 
 	public static void main(String[] args) 
 	{
-		for(int i=10;i>=1;i--)
+		/*for(int i=10;i>=1;i--)
 		System.out.println(i);
-	}
+	}*/
 	
 	// nested for loop
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			if(j==3)
+			System.out.println(i+""+j);
+		}
+	}
+	}
+}
 	
 
-}
 
 
